bf480edbabc1229660393ab07cbf614a05257488
DiggingSteve <doublefly@live.cn>
2022-03-30 17:26:54 +0800

    2

--- SpringCloudWecan/trunk/springCloudWecan/dao/daoFee/src/main/java/com/wecan/daofee/mapper/ViewFreightRoutingMapper.java
+++ SpringCloudWecan/trunk/springCloudWecan/dao/daoFee/src/main/java/com/wecan/daofee/mapper/ViewFreightRoutingMapper.java
@@ -23,7 +23,7 @@ public interface ViewFreightRoutingMapper extends BaseMapper<ViewFreightRouting>
             "fee.area,\n" +
             "fee.gid,\n" +
             "fee.remark,\n" +
-            "truck.ddg ,\n" +
+            "case when truckHs.twocode is  null then '' else truck.ddg end ddg ,\n" +
             "truckHs.twocode as truckTwocode,\n" +
             "fee.hbh, " +
             "truck.min as truckMin, " +
@@ -33,7 +33,7 @@ public interface ViewFreightRoutingMapper extends BaseMapper<ViewFreightRouting>
             "LEFT JOIN Ex_FeeAircompany as hs on hs.feeid=fee.guid\n" +
             "left join Ex_TruckFee as truck on truck.mdg=fee.mdg  and  truck.isdel=1 and truck.wageinout= 1\n" +
             "left join Ex_TruckFeeAircompany as truckHs on truckHs.feeid=truck.guid and truckHs.twocode=hs.twocode and truckHs.isdel=1  \n" +
-            " where  fee.isdel=1 and hs.isdel=1 and truckHs.twocode=hs.twocode \n" + //到达港和航司是必选条件 因为ddg匹配上fee的数据 会混入二字码为null的数据需要剔除
+            " where  fee.isdel=1 and hs.isdel=1 \n" + //到达港和航司是必选条件 因为ddg匹配上fee的数据 会混入二字码为null的数据需要剔除
             ")as a " +
             "${ew.customSqlSegment}\n")
         //"${orderby}")
