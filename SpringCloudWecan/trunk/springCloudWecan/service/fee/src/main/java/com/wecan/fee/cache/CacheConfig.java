cf2a48f253c13339c25821b2a22f12d6940ef3ef
DiggingSteve <doublefly@live.cn>
2022-04-19 16:15:20 +0800

    []

--- SpringCloudWecan/trunk/springCloudWecan/service/fee/src/main/java/com/wecan/fee/cache/CacheConfig.java
+++ SpringCloudWecan/trunk/springCloudWecan/service/fee/src/main/java/com/wecan/fee/cache/CacheConfig.java
@@ -18,7 +18,7 @@ public class CacheConfig {
     public Cache<String, Object> caffeineCache() {
         return Caffeine.newBuilder()
                 // 设置最后一次写入或访问后经过固定时间过期
-                .expireAfterWrite(10, TimeUnit.SECONDS)
+                .expireAfterWrite(1, TimeUnit.DAYS)
                 // 初始的缓存空间大小
                 .initialCapacity(100)
                 // 缓存的最大条数
