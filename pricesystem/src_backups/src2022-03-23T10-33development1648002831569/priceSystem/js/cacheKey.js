1ef4e33527fbda575928a6459ece39c4e50c7525
Steve <doublefly@live.cn>
2022-04-21 17:07:37 +0800

    1

--- pricesystem/src_backups/src2022-03-23T10-33development1648002831569/priceSystem/js/cacheKey.js
+++
@@ -1,4 +0,0 @@
-const localStorageKey={
-    weightDiff:"weightDiff"//重差参数
-};
-export default localStorageKey;
--- pricesystem/src_backups/src2022-03-24T10-24development1648088632104/priceSystem/js/cardBlock.js
+++
@@ -1,3 +0,0 @@
-const cardTitle={packageType:"散托盘设置",weight:"重量等级参数设置",vol:"货型参数设置",cus:"客户参数设置"}
-
-export {cardTitle}
