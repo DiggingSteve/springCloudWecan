82caf260a6dfaee337f8160e3313cd637f154114
DiggingSteve <doublefly@live.cn>
2022-05-17 10:36:51 +0800

    []

--- pricesystem/src/priceSystem/priceFreight.vue
+++ pricesystem/src/priceSystem/priceFreight.vue
@@ -520,11 +520,11 @@
                   <template>
                     {{
                       void(cusSpan=item instanceof Array ? item.cus.reduce.j((pre,current)=>{
-                        return priceObj.cusArr[pre].title+"\\"+priceObj.cusArr[current].title
+                        ;return priceObj.cusArr[pre].title+"\\"+priceObj.cusArr[current].title
                       }):priceObj.cusArr[item.cus].title)
 
                           void(pSpan=item instanceof Array ? item.p.reduce.j((pre,current)=>{
-                        return priceObj.packageTypeArr[pre].title+"\\"+priceObj.packageTypeArr[current].title
+                        ;return priceObj.packageTypeArr[pre].title+"\\"+priceObj.packageTypeArr[current].title
                       }):priceObj.packageTypeArr[item.p].title)
                     }}
                   </template>
