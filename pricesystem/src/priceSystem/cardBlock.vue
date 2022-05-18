377f98d9d1267bee75be0366cbf886beacc366e5
DiggingSteve <doublefly@live.cn>
2022-05-18 21:01:25 +0800

    []

--- pricesystem/src/priceSystem/cardBlock.vue
+++ pricesystem/src/priceSystem/cardBlock.vue
@@ -29,7 +29,7 @@
     <div class="card-wrap">
       <div
         v-for="(item, index) in dataArr"
-        v-if="(item.title != 'MIN')&& (item.isSameAsBase==false)"
+        v-if="(item.title != 'MIN')&& (item.isSameAsBase==false||item.isDefault==1)"
         v-show="item.isAdd || item.isSelect"
         class="card"
         v-bind:class="{ active: index == indexSelf ? true : false }"
