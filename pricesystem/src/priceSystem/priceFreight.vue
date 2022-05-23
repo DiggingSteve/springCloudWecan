2abed1e487d3deb65b9b6e2e831cde4ba19f88ba
DiggingSteve <doublefly@live.cn>
2022-05-23 16:22:38 +0800

    []

--- pricesystem/src/priceSystem/priceFreight.vue
+++ pricesystem/src/priceSystem/priceFreight.vue
@@ -149,7 +149,6 @@
               :placeholder="'可多选'"
               type="inline"
               required="tip"
-              @change="priceObj.queryMatchTruck()"
               :modelval.sync="priceObj.twoCode"
             ></pageSelectMultiple>
           </div>
