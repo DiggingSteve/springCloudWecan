29ab47fdb40b41271cf99086c9f5b25a844f919e
DiggingSteve <doublefly@live.cn>
2022-05-23 17:33:42 +0800

    []

--- pricesystem/src/priceSystem/js/priceFreight.js
+++ pricesystem/src/priceSystem/js/priceFreight.js
@@ -644,9 +644,9 @@ class priceFreightView extends BaseService {
         Promise.all(task).then(()=>{
             if (this.truckAlertArr.length > 0) {
                 //提示
-                twocodeStr = this.truckAlertArr.reduce((pre, cur, index) => {
-                    return pre.twocode + (index > 0 ? "," : "") + cur.twocode;
-                }, '')
+                twocodeStr = this.truckAlertArr.reduce((pre, cur) => {
+                    return pre.twocode + "," + cur.twocode;
+                })
                 let txt = `${this.zzg}至${this.mdg}的航司二字码${twocodeStr}已维护卡车转运费，具体可至"目的港卡车转运费"页面中查看`;
                 this.vueInstance.$alert(txt, '提示', {
                     confirmButtonText: '确定',
